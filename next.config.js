2e59c9d0f85a94d242d923a33b854a1b248dfe30
gazi-faysal-jubayer <gazi.faysal.jubayer@gmail.com>
2025-06-27 05:42:39 +0600

    cfgfdg

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   output: 'export',
+  allowedDevOrigins: [
+    'http://localhost:3000',
+    'http://192.168.0.103:3000'
+  ],
 };
 
 module.exports = nextConfig; 
